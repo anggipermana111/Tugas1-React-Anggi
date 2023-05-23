382874c84841d8cd605a3109c474d6bfe51f283a
Anggi Permana <anggipermana490@gmail.com>
2023-05-23 10:41:43 +0700

    final

--- soal-2.js
+++ soal-2.js
@@ -5,4 +5,14 @@ const arrow = (buas, jinak) => {
   return [...buas, ...jinak];
 };
 
+const makanan = (jinak) => {
+  let kucing, kelinci, ayam;
+  [kucing, kelinci, ayam] = hewanJinak;
+
+  return "Ikan : " + kucing + "\nWortel :" + kelinci + "\nBeras " + ayam;
+};
+
 console.log(arrow(hewanBuas, hewanJinak));
+setTimeout(() => {
+  console.log(makanan(hewanJinak));
+}, 5000);
